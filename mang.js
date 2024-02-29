de5a8f2f4a5c4d2efe4c3207e52483f2ee88bf56
tho <ldtho1604@gmail.com>
2024-02-29 15:43:42 +0700

    mang

---
+++ mang.js
@@ -0,0 +1,20 @@
+1; //Lý thuyết cơ bản về mảng
+const code = ["HTML", "css", "js", "python"];
+// code.join(",");
+// code.push("tho");
+// code.pop();
+// code.shift();
+// code.unshift("tho");
+// code.splice(1, 0, "ok");
+document.write(code);
+
+// console.log(code.join()); //chuyển mảng thành string
+// console.log(code.pop()); //xóa phần tử cuối mảng
+// console.log(code.push("nodejs")); //thêm 1 phần tử vào cuối mảng và trả về mảng mới
+// console.log(code.shift()); //xóa phần tử đầu mảng
+// console.log(code.unshift("reactjs")); ///thêm 1 hoặc nhiều phần tử và đầu mảng
+// console.log(code.splice(0, 1, "tho")); ///xóa hoăcj thêm phần tử vào mảng
+//concat()nối 2 mảng với nhau
+// const code1 = [1, 2, 3, 4, 5];
+// console.log(code.concat(code1));
+// console.log(code.slice(0, 2)); //cắt các phần tử ko làm ảnh hưởng đến mảng ban đầu
